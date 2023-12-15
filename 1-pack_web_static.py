4a6acd10ead257f993b635a3c1a011aab52b4a0a
Ajibose <ajiboseibrahim12@gmail.com>
2023-12-15 21:29:55 +0000

    Task1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -4,14 +4,14 @@
 """
 from datetime import datetime
 from fabric.api import local
-import os
 
 
 def do_pack():
     """generates a .tgz archive from the contents of the web_static folder"""
     currenttime = datetime.now().strftime("%Y%m%d%H%M%S")
 
-    os.makedirs("versions", exist_ok=True)
+    # os.makedirs("versions", exist_ok=True)
+    local("mkdir -p versions")
     res = local(f"tar -cf versions/web_static_{currenttime}.tgz web_static")
     if res.succeeded:
         return res
