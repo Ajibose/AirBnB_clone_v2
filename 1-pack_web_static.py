7a1afb3d6e58875976c2dc6170c9ea24d3cb2afd
Ajibose <ajiboseibrahim12@gmail.com>
2023-12-15 11:42:38 +0000

    Task1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -3,14 +3,18 @@
     Define a funcion that generates a .tgz archive from the contents 
 """
 from datetime import datetime
-from fabric.api import run
+from fabric.api import loca;
+import os
+
 
 def do_pack():
     """generates a .tgz archive from the contents of the web_static folder"""
     currenttime = datetime.now().strftime("%Y%m%d%H%M%S")
-    res = run(f"tar -cf web_static_{currenttime}.tgz web_static/")
+
+    os.makedirs("versions", exist_ok=True)
+    res = run(f"tar -cf versions/web_static_{currenttime}.tgz web_static/")
     if res.exited == 0:
         return None
     else:
-        return f"web_static_{currenttime}.tgz"
+        return res
     
