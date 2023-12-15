590e9fedee9c2809b9c3acc1ac583c380103284c
Ajibose <ajiboseibrahim12@gmail.com>
2023-12-15 21:31:22 +0000

    Task1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -10,9 +10,8 @@ def do_pack():
     """generates a .tgz archive from the contents of the web_static folder"""
     currenttime = datetime.now().strftime("%Y%m%d%H%M%S")
 
-    # os.makedirs("versions", exist_ok=True)
     local("mkdir -p versions")
-    res = local(f"tar -cf versions/web_static_{currenttime}.tgz web_static")
+    res = local(f"tar -cf versions/web_static_{currenttime}.tgz web_static/")
     if res.succeeded:
         return res
     else:
