b19f9dfc8bbc8bb5ca1cc144c98f8186af6edfa7
Ajibose <ajiboseibrahim12@gmail.com>
2024-01-01 20:13:35 +0000

    Task2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""A script that starts a Flask web application"""
+
+
+from flask import Flask
+
+
+app = Flask("__name__")
+
+
+@app.route("/", strict_slashes=False)
+def root_HBNB():
+    """View function for home page"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """View for hbnb page"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_page(text):
+    """View for c page"""
+    return f"C {text.replace('_', ' ')}"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
