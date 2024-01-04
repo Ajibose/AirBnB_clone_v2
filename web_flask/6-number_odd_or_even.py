67457112bdefc42e8c2a69e84b9f9258c7b77553
Ajibose <ajiboseibrahim12@gmail.com>
2024-01-04 14:57:14 +0000

    Task6

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -4,8 +4,7 @@
 
 from flask import Flask, render_template
 
-app = Flask("__name__")
-# template_folder='web_flask/templates'
+app = Flask("__name__", template_folder='web_flask/templates')
 
 
 @app.route("/", strict_slashes=False)
